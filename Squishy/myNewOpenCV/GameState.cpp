6e42bd94ed001801d12bd904523d7d49f8985d22
vincent advocaat <vincentadvocaat@gmail.com>
2013-05-31 13:49:27 +0200

    gamestate
    skeleton for gamestate
    lives and points

---
+++ Squishy/myNewOpenCV/GameState.cpp
@@ -0,0 +1,43 @@
+#include "GameState.h"
+
+int lives;
+int points;
+
+GameState::GameState(void)
+{
+	lives = 3;
+	points = 0;
+}
+
+
+GameState::~GameState(void)
+{
+}
+
+void GameState::die()
+	{
+		lives--;
+		if(lives >= 0)
+		{
+			//respawn
+		}
+		if(lives < 0)
+		{
+			//open game over screen
+		}
+	}
+
+	void GameState::regen()
+	{
+		lives++;
+	}
+
+	void GameState::addPoint()
+	{
+		points++;
+	}
+
+	void GameState::addPoints(int number)
+	{
+		points += number;
+	}
---
+++ Squishy/myNewOpenCV/GameState.h
@@ -0,0 +1,12 @@
+#pragma once
+class GameState
+{
+public:
+	GameState(void);
+	~GameState(void);
+	void die();
+	void regen();
+	void addPoint();
+	void addPoints(int);
+
+};
